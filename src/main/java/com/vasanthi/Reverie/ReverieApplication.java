2b4be7fc65b15c2720e612a887e17708eac3fdce
VasanthiSivasankar <vasutha2007@gmail.com>
2026-05-23 20:24:17 +0530

    spring setup

---
+++ src/main/java/com/vasanthi/Reverie/ReverieApplication.java
@@ -0,0 +1,13 @@
+package com.vasanthi.Reverie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReverieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReverieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vasanthi/Reverie/ReverieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasanthi.Reverie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReverieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
